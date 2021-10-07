a2f2bde1aa070c8aa8f6ab2506749df7c9ca4c30
Michiel <michielvries95@gmail.com>
2021-10-07 14:51:43 +0200

    complete makeover of index.php - added 3 stock images

--- php/src/index.php
+++ php/src/index.php
@@ -1,79 +1,78 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <title>Bootstrap Tutorial Sample Page</title>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="bootstrap/dist/css/bootstrap.min.css">
-<!--    <link rel="stylesheet" type="text/css" href="main.css"> -->
-    
+  <title>Bootstrap Example</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js"></script>
 </head>
-
 <body>
-    <nav class="navbar navbar-expand-md">
-        <a class="navbar-brand" href="#">Logo</a>
-        <button class="navbar-toggler navbar-dark" type="button" data-toggle="collapse" data-target="#main-navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="main-navigation">
-            <ul class="navbar-nav">
-                <li class="nav-item">
-                    <a class="nav-link active" href="#">Home</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">About</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link disabled" href="#">Contact</a>
-                </li>
-            </ul>
+
+<nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+  <div class="container">
+    <a class="navbar-brand" href="#">Logo</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavbar">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="collapsibleNavbar">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link" href="#">Link</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Link</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Link</a>
+        </li>    
+      </ul>
+    </div>
+  </div>
+</nav>
+
+
+<div class="container">
+    <div id="demo" class="carousel slide" data-bs-ride="carousel">
+
+    <!-- Indicators/dots -->
+    <div class="carousel-indicators">
+        <button type="button" data-bs-target="#demo" data-bs-slide-to="0" class="active"></button>
+        <button type="button" data-bs-target="#demo" data-bs-slide-to="1"></button>
+        <button type="button" data-bs-target="#demo" data-bs-slide-to="2"></button>
+    </div>
+    
+    <!-- The slideshow/carousel -->
+    
+    <div class="carousel-inner">
+        <div class="carousel-item active">
+        <img src="/img/document.jpeg" alt="Document" class="d-block" style="width:100%">
         </div>
-    </nav>
+        <div class="carousel-item">
+        <img src="/img/files.jpeg" alt="Files" class="d-block" style="width:100%">
+        </div>
+        <div class="carousel-item">
+        <img src="/img/pen.jpeg" alt="Pen and paper" class="d-block" style="width:100%">
+        </div>
+    </div>
+    
+    <!-- Left and right controls/icons -->
+    <button class="carousel-control-prev" type="button" data-bs-target="#demo" data-bs-slide="prev">
+        <span class="carousel-control-prev-icon"></span>
+    </button>
+    <button class="carousel-control-next" type="button" data-bs-target="#demo" data-bs-slide="next">
+        <span class="carousel-control-next-icon"></span>
+    </button>
+    </div>
 
+</div>
 
-    <script src="bootstrap//dist/js/bootstrap.min.js"></script>
+<div class="container mt-3">
+  <h3>Collapsible Navbar</h3>
+  <p>In this example, the navigation bar is hidden on small screens and replaced by a button in the top right corner (try to re-size this window).</p>
+  <p>Only when the button is clicked, the navigation bar will be displayed.</p>
+  <p>Tip: You can also remove the .navbar-expand-md class to ALWAYS hide navbar links and display the toggler button.</p>
+</div>
+
 </body>
-	
 </html>
-
-<?php
-//These are the defined authentication environment in the db service
-
-// The MySQL service named in the docker-compose.yml.
-$host = 'db';
-
-// Database use name
-$user = 'MYSQL_USER';
-
-//database user password
-$pass = 'MYSQL_PASSWORD';
-
-// database name
-$mydatabase = 'MYSQL_DATABASE';
-// check the mysql connection status
-
-$conn = new mysqli($host, $user, $pass, $mydatabase);
-
-// select query
-$sql = 'SELECT * FROM users';
-
-if ($result = $conn->query($sql)) {
-    while ($data = $result->fetch_object()) {
-        $users[] = $data;
-    }
-}
-
-foreach ($users as $user) {
-    echo "<br>";
-    echo $user->username . " " . $user->password;
-    echo "<br>";
-}
-
-echo "<hr>";
-
-
-echo(boolval(rand(0,1)));
-
-echo "<hr>";
-
-?>
